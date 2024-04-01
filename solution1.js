f64ef5b015a376aeaf4c6185a359889ae371e319
roy wahome <roy.njuguna@student.moringaschool.com>
2024-04-01 10:25:49 +0300

    swapCase

---
+++ solution1.js
@@ -0,0 +1,15 @@
+function swapCase(str) {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === char.toUpperCase()) {
+            result += char.toLowerCase();
+        } else {
+            result += char.toUpperCase();
+        }
+    }
+    return result;
+}
+
+
+console.log(swapCase('The Quick Brown Fox')); 
